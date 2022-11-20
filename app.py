6e365b66a5006b4f603cadc1078f855b3df00858
RENGA RAJAN K <maharengarajan46@gmail.com>
2022-11-20 21:04:25 +0530

    logging and exception completed

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from housing.logger import logging
+import sys
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    logging.info("we are testing logging module once again")
     return "CI CD pipeline established"
 
 if __name__=="__main__":
